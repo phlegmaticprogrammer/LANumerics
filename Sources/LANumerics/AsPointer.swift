a9fc2bb6dbb185291c514e0fe081d91608b414e8
Steven Obua <steven@recursivemind.com>
2020-05-18 10:30:27 +0100

    Added throws/rethrows signatures to Functional.

--- Sources/LANumerics/AsPointer.swift
+++ Sources/LANumerics/AsPointer.swift
@@ -1,14 +1,14 @@
-func asMutablePointer<T, R>(_ array : inout [T], block : (UnsafeMutablePointer<T>) -> R) -> R {
+func asMutablePointer<T, R>(_ array : inout [T], block : (UnsafeMutablePointer<T>) throws -> R) rethrows -> R {
     let count = array.count
-    return array.withUnsafeMutableBytes { ptr in
-        block(ptr.baseAddress!.bindMemory(to: T.self, capacity: count))
+    return try array.withUnsafeMutableBytes { ptr in
+        try block(ptr.baseAddress!.bindMemory(to: T.self, capacity: count))
     }
 }
 
-func asPointer<T, R>(_ array : [T], block : (UnsafePointer<T>) -> R) -> R {
+func asPointer<T, R>(_ array : [T], block : (UnsafePointer<T>) throws -> R) rethrows -> R {
     let count = array.count
-    return array.withUnsafeBytes { ptr in
-        block(ptr.baseAddress!.bindMemory(to: T.self, capacity: count))
+    return try array.withUnsafeBytes { ptr in
+        try block(ptr.baseAddress!.bindMemory(to: T.self, capacity: count))
     }
 }
 
--- Sources/LANumerics/Functional.swift
+++ Sources/LANumerics/Functional.swift
@@ -2,19 +2,19 @@ import Foundation
 
 public extension Matrix {
     
-    func map<E>(_ transform : (Element) -> E) -> Matrix<E> {
-        return Matrix<E>(rows: _rows, columns: _columns, elements: elements.map(transform))
+    func map<E>(_ transform : (Element) throws -> E) rethrows -> Matrix<E> {
+        return Matrix<E>(rows: _rows, columns: _columns, elements: try elements.map(transform))
     }
 
-    func combine<E, F>(_ other : Matrix<E>, _ using : (Element, E) -> F) -> Matrix<F> {
+    func combine<E, F>(_ other : Matrix<E>, _ using : (Element, E) throws -> F) rethrows -> Matrix<F> {
         precondition(hasSameDimensions(other))
         let C = count
         var elems = [F](repeating: F.zero, count: C)
-        asPointer(self.elements) { elems1 in
-            asPointer(other.elements) { elems2 in
-                asMutablePointer(&elems) { elems in
+        try asPointer(self.elements) { elems1 in
+            try asPointer(other.elements) { elems2 in
+                try asMutablePointer(&elems) { elems in
                     for i in 0 ..< C {
-                        elems[i] = using(elems1[i], elems2[i])
+                        elems[i] = try using(elems1[i], elems2[i])
                     }
                 }
             }
@@ -22,28 +22,28 @@ public extension Matrix {
         return Matrix<F>(rows: _rows, columns: _columns, elements: elems)
     }
     
-    func fold<F>(_ start : F, _ using : (F, Element) -> F) -> F {
+    func fold<F>(_ start : F, _ using : (F, Element) throws -> F) rethrows -> F {
         var result = start
         for elem in elements {
-            result = using(result, elem)
+            result = try using(result, elem)
         }
         return result
     }
     
-    func fold(_ using : (Element, Element) -> Element) -> Element {
-        return fold(Element.zero, using)
+    func fold(_ using : (Element, Element) throws -> Element) rethrows -> Element {
+        return try fold(Element.zero, using)
     }
     
-    func forall(_ cond : (Element) -> Bool) -> Bool {
+    func forall(_ cond : (Element) throws -> Bool) rethrows -> Bool {
         for elem in elements {
-            guard cond(elem) else { return false }
+            guard try cond(elem) else { return false }
         }
         return true
     }
 
-    func exists(_ cond : (Element) -> Bool) -> Bool {
+    func exists(_ cond : (Element) throws -> Bool) rethrows -> Bool {
         for elem in elements {
-            if cond(elem) { return true }
+            if try cond(elem) { return true }
         }
         return false
     }
