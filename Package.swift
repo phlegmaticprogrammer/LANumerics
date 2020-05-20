097d3ab3862e6748e3374ba2a482762cce54a100
Steven Obua <steven@recursivemind.com>
2020-05-20 02:46:10 +0100

    Added dependency to swift-numerics

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
             targets: ["LANumerics"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-numerics", from: "0.0.5")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -20,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "LANumerics",
-            dependencies: []),
+            dependencies: [.product(name: "Numerics", package: "swift-numerics")]),
         .testTarget(
             name: "LANumericsTests",
             dependencies: ["LANumerics"]),
